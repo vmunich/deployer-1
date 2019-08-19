59bd2004fa74ffb3211b18346024af1f92ca071a
Vini Münich <viniciusmunich@gmail.com>
2019-08-19 18:43:44 -0400

    wip

---
+++ app/update-core.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+update_core_handle()
+{
+	local UPSTREAM_VERSION=$(curl -s https://raw.githubusercontent.com/ArkEcosystem/core/master/packages/core/package.json | jq -r '.version')
+	
+	cd $HOME/core-bridgechain
+	git checkout -b update/"$UPSTREAM_VERSION"
+	git merge upstream/master
+
+	update_core_resolve_conflicts
+	update_core_change_block_reward_from_number_to_string
+	update_core_update_package_json
+
+	git add --all
+	git commit --no-verify -m 'chore: upgrade bridgechain'
+	git push --no-verify
+
+}
+
+update_core_add_upstream_remote()
+{
+	heading "Fetching from upstream..."
+	cd $HOME/core-bridgechain
+	git remote add upstream https://github.com/ArkEcosystem/core.git > /dev/null 2>&1
+	git fetch upstream
+}
+
+update_core_check_bridgechain_version()
+{
+	local CHAIN_VERSION=$(jq -r '.version' $HOME/core-bridgechain/packages/core/package.json)
+	local UPSTREAM_VERSION=$(curl -s https://raw.githubusercontent.com/ArkEcosystem/core/master/packages/core/package.json | jq -r '.version')
+
+	# read -p "Would you like to update your bridgechain? [y/N]: " choice
+
+	# if [[ "$choice" =~ ^(yes|y|Y) ]]; then
+	#     choice=""
+	#     while [[ ! "$choice" =~ ^(yes|y|Y) ]] ; do
+	#     	#
+	#         read -p "Proceed? [y/N]: " choice
+	#     done
+	fi
+}
+
+update_core_resolve_conflicts()
+{
+	git checkout --ours packages/crypto/src/networks/devnet/genesisBlock.json
+	git checkout --ours packages/crypto/src/networks/devnet/milestones.json
+	git checkout --ours packages/crypto/src/networks/mainnet/exceptions.json
+	git checkout --ours packages/crypto/src/networks/mainnet/genesisBlock.json
+	git checkout --ours packages/crypto/src/networks/mainnet/milestones.json
+	git checkout --ours packages/crypto/src/networks/testnet/genesisBlock.json
+	git checkout --ours packages/crypto/src/networks/testnet/milestones.json
+	git checkout --theirs packages/core/bin/config/mainnet/plugins.js
+	git checkout --theirs packages/core/bin/config/testnet/plugins.js
+	git checkout --ours install.sh
+}
+
+update_core_change_block_reward_from_number_to_string()
+{
+	tmp=$(mktemp)
+	jq '.reward = "0"' packages/crypto/src/networks/mainnet/genesisBlock.json > "$tmp" && mv "$tmp" packages/crypto/src/networks/mainnet/genesisBlock.json
+	tmp=$(mktemp)
+	jq '.reward = "0"' packages/crypto/src/networks/devnet/genesisBlock.json > "$tmp" && mv "$tmp" packages/crypto/src/networks/devnet/genesisBlock.json
+	tmp=$(mktemp)
+	jq '.reward = "0"' packages/crypto/src/networks/testnet/genesisBlock.json > "$tmp" && mv "$tmp" packages/crypto/src/networks/testnet/genesisBlock.json
+}
+
+update_core_update_package_json()
+{
+	oldPackageJson=$(mktemp)
+	git checkout --ours packages/core/package.json && cat packages/core/package.json > "$oldPackageJson" && git checkout --theirs packages/core/package.json
+
+	tmp=$(mktemp)
+	jq --arg var "$(jq -r '.name' "$oldPackageJson")" '.name = $var' packages/core/package.json > "$tmp" && mv "$tmp" packages/core/package.json
+
+	tmp=$(mktemp)
+	jq --argjson bin "$(jq -r '.bin' "$oldPackageJson")" '.scripts += $bin' packages/core/package.json > "$tmp" && mv "$tmp" packages/core/package.json
+
+	tmp=$(mktemp)
+	jq --arg var "$(jq -r '.description' "$oldPackageJson")" '.description = $var' packages/core/package.json > "$tmp" && mv "$tmp" packages/core/package.json
+
+	tmp=$(mktemp)
+	jq --argjson var "$(jq -r '.bin' "$oldPackageJson")" '.bin = $var' packages/core/package.json > "$tmp" && mv "$tmp" packages/core/package.json
+
+	tmp=$(mktemp)
+	jq --arg var "$(jq -r '.bin' "$oldPackageJson")" '.scripts = $var' packages/core/package.json > "$tmp" && mv "$tmp" packages/core/package.json
+
+	tmp=$(mktemp)
+	jq --arg var "$(jq -r '.oclif.bin' "$oldPackageJson")" '.oclif.bin = $var' packages/core/package.json > "$tmp" && mv "$tmp" packages/core/package.json
+}
