b1f2b43f5959062020c9b1dd1273785ac03fdab5
Vini Münich <viniciusmunich@gmail.com>
2019-08-19 20:26:12 -0400

    wip

--- app/update-core.sh
+++ app/update-core.sh
@@ -110,13 +110,13 @@ update_core_update_package_json()
 	jq --arg var "$(jq -r '.name' "$oldPackageJson")" '.name = $var' packages/core/package.json \
 	>| packages/core/package.json.tmp && mv packages/core/package.json.tmp packages/core/package.json
 
-	jq -r --argjson bin "$(jq -r '.bin' "$oldPackageJson")" '.scripts += $bin' packages/core/package.json \
+	jq --argjson var "$(jq -r '.bin' "$oldPackageJson")" '.bin = $var' packages/core/package.json \
+	>| packages/core/package.json.tmp && mv packages/core/package.json.tmp packages/core/package.json
+
+	jq -r --argjson var "$(jq -r '.bin' "$oldPackageJson")" '.scripts += $var' packages/core/package.json \
 	>| packages/core/package.json.tmp && mv packages/core/package.json.tmp packages/core/package.json
 
 	jq --arg var "$(jq -r '.description' "$oldPackageJson")" '.description = $var' packages/core/package.json \
-	>| packages/core/package.json.tmp && mv packages/core/package.json.tmp packages/core/package.json
-
-	jq --argjson var "$(jq -r '.bin' "$oldPackageJson")" '.bin = $var' packages/core/package.json \
 	>| packages/core/package.json.tmp && mv packages/core/package.json.tmp packages/core/package.json
 
 	jq --arg var "$(jq -r '.bin' "$oldPackageJson")" '.scripts = $var' packages/core/package.json \
